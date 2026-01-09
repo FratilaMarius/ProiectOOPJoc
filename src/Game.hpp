890c8f09701d9da609441449341199db59be6447
FratilaMarius <marius.fratila123@gmail.com>
2026-01-09 10:17:12 +0200

    restructuring main

---
+++ src/Game.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <chrono>
+#include <thread>
+#include <SFML/Graphics.hpp>
+
+#include "Player.hpp"
+#include "Enemy.hpp"
+#include "Utility.hpp"
+#include "Room.hpp"
+#include "Labyrinth.hpp"
+#include "TextureLoader.hpp"
+#include "Exceptions.hpp"
+#include "UI.hpp"
+#include "Miscellaneous.hpp"
+class Game{
+  public:
+    Game(int W, int h, int a, int b, sf::RenderWindow &window);
+    private:
+};
